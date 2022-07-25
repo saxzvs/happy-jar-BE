aaae6bd99dbf26f1e0fc080977769322b5898201
Jieun <saxzvs@gmail.com>
2022-07-25 19:50:55 +0900

    Chore. log4j2 추가, default Test class 추가

--- src/main/java/com/mamasclub/happyjar/api/v1/domain/entity/User.java
+++ src/main/java/com/mamasclub/happyjar/api/v1/domain/entity/User.java
@@ -23,13 +23,26 @@ public class User extends CommonEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userNo;
-    @ApiModelProperty(value = "유저ID") @Column(name="USER_ID") private String userId;
-    @ApiModelProperty(value = "인증타입") @Column(name="AUTH_TYPE") private String authType;
-    @ApiModelProperty(value = "비밀번호") @Column private String userPw;
-    @ApiModelProperty(value = "사용자명") @Column private String userNm;
-    @ApiModelProperty(value = "이메일") @Column private String email;
-    @ApiModelProperty(value = "접속토큰") @Column private String access_token;
-    @ApiModelProperty(value = "등급") @Column private String userGrade;
-    @ApiModelProperty(value = "활성화여부") @Column private String activateYn;
-    @ApiModelProperty(value = "사용여부") @Column private String useYn;
+    @ApiModelProperty(value = "유저UUID")
+    private String userUuid;
+    @ApiModelProperty(value = "유저ID") @Column(name="USER_ID")
+    private String userId;
+    @ApiModelProperty(value = "인증타입") @Column(name="AUTH_TYPE")
+    private String authType;
+    @ApiModelProperty(value = "비밀번호") @Column
+    private String userPw;
+    @ApiModelProperty(value = "사용자명") @Column
+    private String userNm;
+    @ApiModelProperty(value = "인증키") @Column
+    private String authKey;
+    @ApiModelProperty(value = "이메일") @Column
+    private String email;
+    @ApiModelProperty(value = "접속토큰") @Column
+    private String access_token;
+    @ApiModelProperty(value = "등급") @Column
+    private String userGrade;
+    @ApiModelProperty(value = "활성화여부") @Column
+    private String activateYn;
+    @ApiModelProperty(value = "사용여부") @Column
+    private String useYn;
 }
